2e425d4e85c0ea1a1e468036baa5816e7c90957d
ftoroc <ftoroc.tsoft@bancochile.cl>
2021-04-01 17:25:23 -0300

    first commit

---
+++ src/main/java/Common/constants/Navegador.java
@@ -0,0 +1,5 @@
+package Common.constants;
+
+public enum Navegador {
+    Firefox, Chrome, IExplorer, Edge
+}
---
+++ src/main/java/Common/reporter/EstadoPrueba.java
@@ -0,0 +1,10 @@
+package Common.reporter;
+
+public enum EstadoPrueba {
+    PASSED,
+    FAILED,
+    WARNING;
+
+    private EstadoPrueba() {
+    }
+}
